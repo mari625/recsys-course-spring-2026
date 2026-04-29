f25a009aa9bccd0a0ccbcc9b42dd63a18a00100d
vboxuser <vboxuser@project.myguest.virtualbox.org>
2026-04-29 17:43:33 +0300

    Update hw

--- botify/botify/recommenders/hw_recommender.py
+++ botify/botify/recommenders/hw_recommender.py
@@ -1,29 +1,3 @@
-#import random
-
-#from .recommender import Recommender
-
-
-#class HWRecommender(Recommender):
-#    def __init__(self, recommendations_redis, catalog, fallback):
-#        self.recommendations_redis = recommendations_redis
-#        self.fallback = fallback
-#        self.catalog = catalog
-#
-#    def recommend_next(self, user: int, prev_track: int, prev_track_time: float) -> int:
-#        recommendations = self.recommendations_redis.get(user)
-#
-#        if recommendations is not None:
-#            tracks = list(self.catalog.from_bytes(recommendations))
-#            #weights = [1.0 / (i + 1) for i in range(len(tracks))]
-#        
-#            #probs = [w / sum(weights) for w in weights]
-#        
-#            #return random.choices(tracks, weights=probs, k=1)[0]
-#            return tracks[0]
-#        else:
-#            return self.fallback.recommend_next(user, prev_track, prev_track_time)
-        
-
 import json
 import pickle
 import random
